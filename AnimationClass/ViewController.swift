8a375cc1b2fc0833f4e97395936305dce3731c5b
Indah <indaahfitria@gmail.com>
2019-05-17 14:23:22 +0700

    Initial Commit

---
+++ AnimationClass/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimationClass
+//
+//  Created by Indah Fitriany on 17/05/19.
+//  Copyright © 2019 Indah Fitriany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
