ef9853b2ad44901a7f21606c8e0fbfe5bdd05157
Indah <indaahfitria@gmail.com>
2019-05-17 15:54:36 +0700

    animasi alpha

--- AnimationClass/ViewController.swift
+++ AnimationClass/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myObject: UIButton!
+    
+
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+     myObject.layer.cornerRadius = myObject.frame.width/2
+        
+        print(myObject.frame)
     }
 
+    
+//    @IBAction func actionButton(_ sender: Any) {
+//        UIView.animate(withDuration: 1){
+//            self.myObject.frame = CGRect(x: 23, y: 100, width: 100, height: 100)
+//
+//            print(self.myObject.frame)
+//    }
+//}
 
-}
+    @IBAction func actionButton(_ sender: Any) {
+        if self.myObject.alpha == 1.0{ UIView.animate(withDuration: 1){
+            
+            self.myObject.alpha = 0.3
+        }
+        }else{
+            UIView.animate(withDuration: 1){
+                self.myObject.alpha = 1
+            }
+        }
+    }
+    
+    
+   
+    }
+    
+    
 
